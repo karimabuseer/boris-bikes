6942f6f84f8cdb630c359cc6dc4ad9c6fecd66d1
karimabuseer <karimabuseer@gmail.com>
2021-06-30 17:16:10 +0100

    created docking bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,13 @@
-require_relative 'Bike'
+require_relative 'bike'
 
 class DockingStation
     def release_bike
         Bike.new
+    end
+    
+    attr_reader :bike
+
+    def dock(bike)
+        @bike = bike
     end
 end
