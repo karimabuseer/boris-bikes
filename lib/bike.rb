e69aa837a02875ad966c04302dc7d3a153db6251
karimabuseer <karimabuseer@gmail.com>
2021-06-30 16:33:52 +0100

    Step 10 completed

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,2 +1,6 @@
+
 class Bike
+    def working?
+        p 'hi'
+    end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,15 @@
 require "docking_station"
+require "bike"
+
+
+
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
+    it 'releases working bikes' do
+        bike = subject.release_bike
+        expect(bike).to respond_to(:working?)
+ end
 end
+
+
+
